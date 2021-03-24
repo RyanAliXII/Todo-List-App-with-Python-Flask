02c4dd00bf34c1d57da14d490826a11638154d40
Ryan Ali <Ryanali456@gmail.com>
2021-03-24 08:00:23 +0800

    CRUD implemented

--- controllers/todos_controller.py
+++ controllers/todos_controller.py
@@ -7,11 +7,11 @@ todos = [];
 @todos_blueprint.route('/')
 def read_todos():  
  todo = Todo()
- response = todo.read()
- return jsonify(dict(todos=todos))
+ result = todo.read()
+ return jsonify(dict(todos=result))
 
 @todos_blueprint.route('/',methods=['POST'])
-def create_todos():
+def create_todo():
 
     data = request.get_json(silent=True)
     title = data.get('title')
@@ -22,12 +22,17 @@ def create_todos():
     todo.set_body(body)
     todo.save();
     
-    todos.append({
-        "id":len(todos) + 1,
-        "title":title,
-        "body":body
-    })
-
     return {"message": "OK"}
 
-    return jsonify("hello world")
+@todos_blueprint.route('/<id>',methods=['DELETE'])
+def delete_todo(id):
+    todo = Todo()
+    todo.delete(id)
+    return {"message":"OK"}
+
+@todos_blueprint.route('/<id>',methods=['PATCH'])
+def update_todo(id):
+    todo = Todo()
+    todo.update(id)
+    return {"message":"OK"}
+
--- models/todos_model.py
+++ models/todos_model.py
@@ -1,22 +1,40 @@
 from flask import jsonify;
+from bson.objectid import ObjectId
 from models.database_model import Database
 
 class Todo:
         
     def read(self):
-        db = Database();
-        
-        return jsonify(
-        todos=[dict(id=1, title = "Todo 1",description = "First todo in list" ,isDone = False), dict(id=2, title = "Todo 2",description = "Second todo in list" ,isDone = False)]
-        )
+        database = Database();
+        collection = database.get_db().get_collection('todos')
+        results = collection.find({})
+        result_list = []
+        for result in results :
+            todo = {
+                "id": str(result.get('_id')),
+                "title":result.get('title'),
+                "body":result.get('body'),
+                "isDone":result.get('isDone')
+            }
+            print(todo)
+            result_list.append(todo)
+
+        return result_list
     def save(self): 
-         todo = {"title":self.title, "body": self.body}
+         todo = {"title":self.title, "body": self.body, "isDone": False}
          database = Database()
          collection = database.get_db().get_collection('todos')
          collection.insert_one(todo)
+        
          return dict(message="OK")
-    def update(self):
-     return "Hello world"
+    def delete(self,id):
+        database = Database()
+        collection = database.get_db().get_collection('todos')
+        collection.delete_one({"_id":ObjectId(id)})
+    def update(self,id):
+        database = Database()
+        collection = database.get_db().get_collection('todos')
+        collection.update_one({"_id":ObjectId(id)},{"$set":{"isDone":True}})
     
     def set_title(self,title):
         self.title = title
